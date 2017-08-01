cd57e34411bb220396fee9a1738049505579e4c5
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-01 15:09:42 -0700

    All 5 questions complete and working

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 //use strict statement?
 
 /* TODO
-- Ask the user 5 y/n questions about me
--fix the input validation 
 -style the page
 */
 alert('You are in for a treat, we are going to talk all about MEEEEE!');
 var userName = prompt('What is your name, friend?');
 
+var travel = prompt( 'Do you think that I like to travel?').toLowerCase();
+console.log(userName + ' picked ' + travel + ' for the travel question ');
+if (travel === 'yes' | travel === 'y') {
+  alert('How did you know? Traveling is the point of life in my opinion.');
+}
+else {
+  alert('Wrong. I love traveling.');
+}
+
+var virginIslands = prompt( 'Have I ever been to the Virgin Islands?').toLowerCase();
+console.log(userName + ' picked ' + virginIslands + ' for the virgin islands question.');
+if (virginIslands === 'yes' | virginIslands === 'y') {
+  alert('Haha, no way! What do I look like? A millionaire?? I\'ve been to Big Corn Island in Nicaragua though.');
+}
+else {
+  alert( 'Good one! I haven\'t been there, but hopefully one day I will visit!');
+}
+
 var chocolate = prompt( 'Do I like chocolate? Please answer yes or no' ).toLowerCase();
-console.log(userName + ' picked ' + chocolate + ' for the chocolate question ');
+console.log(userName + ' picked ' + chocolate + ' for the chocolate question.');
 if (chocolate === 'yes' | chocolate === 'y') {
   alert('Very good. Of course I like chocolate!');
 }
@@ -17,12 +33,21 @@ else {
   alert('What? seriously?! Why on earth would I not like chocolate?!');
 }
 
-var travel = prompt( 'Do you think that I like to travel?').toLowerCase();
-console.log(userName + ' picked ' + travel + ' for the travel question ');
-if (travel === 'yes' | travel === 'y') {
-  alert('How did you know? Traveling is the point of life in my opinion');
+var code = prompt( 'Do I like to code?').toLowerCase();
+console.log(userName + ' picked ' + code + ' for the code question.');
+if (code === 'yes' | code === 'y' ) {
+  alert('Well, that one was easy, why else would I be making this?');
 }
 else {
-  alert('Wrong. I love traveling.');
+  alert('Whaaaa? Of course I like to code! I want to build things and have zero physical co-ordination.');
+}
+
+var booze = prompt( 'Do I not like beer?').toLowerCase();
+console.log(userName + ' picked ' + booze + ' for the beer question.');
+if (booze === 'yes' | booze === 'y') {
+  alert('Not like beer? How absurd. If class is over, I am probably having one now!');
+}
+else {
+  alert('Didn\'t fall for my trick huh? No, I don\'t not like beer- I LOVE it. In fact, I\'m going to have one now!');
 }
 
